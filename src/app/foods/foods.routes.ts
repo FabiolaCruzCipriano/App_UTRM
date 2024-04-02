5e22797cc4d975cd2602be9cd1c31d5ab6ee986e
FabiolaCruzCipriano <2202185@utrivieramaya.edu.mx>
2024-04-02 15:30:53 -0500

    Corrección

--- src/app/foods/foods.routes.ts
+++ src/app/foods/foods.routes.ts
@@ -1,15 +1,33 @@
-
 import { Route } from "@angular/router";
 import { FoodListComponent } from "./food-list/food-list.component";
 import { FormFoodComponent } from "./form-food/form-food.component";
 import { DetailsFoodComponent } from "./details-food/details-food.component";
 
-// In admin/routes.ts:
 export default [
-   
-    { path: 'food-list', component: FoodListComponent, title: 'Lista de comida' },
-    { path: 'form', component: FormFoodComponent, title: 'Crear comida' },
-    { path: 'form/id', component: FormFoodComponent, title: 'Actualizar comida' },
-    { path: 'details-food/:id', component: DetailsFoodComponent, title: 'Comida detallada' }
-
+    {
+        path: 'food-list',
+        component: FoodListComponent,
+        title: 'Lista de comida'
+    },
+    {
+        path: 'form',
+        component: FormFoodComponent
+    },
+    {
+        path: 'form/:id',
+        component: FormFoodComponent,
+        title: 'Actualizar comida'
+    },
+    {
+        path: 'details-food/:id',
+        component: DetailsFoodComponent,
+        title: 'Detalles de comida'
+    }
 ] satisfies Route[];
+
+
+
+
+
+
+
